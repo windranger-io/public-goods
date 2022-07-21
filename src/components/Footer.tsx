4fa2b677e53d258dc8034165141041a70f8bf055
HagiaSofiya <sofiyashrayber@gmail.com>
2022-07-21 16:17:53 -0400

    chore: footer border and logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,50 @@
+import { Box } from '@chakra-ui/react'
+import Image from 'next/image'
+
 const Footer = () => {
-  return <div>Public Good</div>
+  return (
+    <Box mx={{ base: '16px', md: '60px' }}>
+      <Separator />
+      <Box>
+        <Image
+          alt="Public Goods Logo"
+          src="/images/public-goods.svg"
+          layout="responsive"
+          width={2427}
+          height={450}
+        />
+      </Box>
+    </Box>
+  )
 }
 
 export default Footer
+
+const Separator = () => {
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      justifyContent={'space-between'}
+      alignItems="center"
+    >
+      <Box
+        width="0"
+        height="0"
+        borderTop="6px solid transparent"
+        borderLeft="12px solid rgba(34, 83, 80, 0.1)"
+        borderBottom="6px solid transparent"
+        mr="-1px"
+      />
+      <Box height="1px" width={'100%'} background="rgba(34, 83, 80, 0.1)" />
+      <Box
+        width="0"
+        height="0"
+        borderTop="6px solid transparent"
+        borderRight="12px solid rgba(34, 83, 80, 0.1)"
+        borderBottom="6px solid transparent"
+        ml="-1px"
+      />
+    </Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { Box } from '@chakra-ui/react'
 import Hero from 'components/Hero'
 import Content from 'components/Content'
+import Footer from 'components/Footer'
 
 const Home = () => {
   return (
     <Box>
       <Hero />
       <Content />
+      <Footer />
     </Box>
   )
 }
